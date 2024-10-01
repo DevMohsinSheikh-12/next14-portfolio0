802b84ca700cf0626a892656329f0eef71ec8a0d
Mohsin Sheikh <160018515+DevMohsinSheikh-12@users.noreply.github.com>
2024-10-02 01:10:34 +0500

    Update Herosection.tsx

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from 'next/link';
 import React from 'react'
-import {useTypewriter,Cursor,  } from "react-simple-typewriter"
+import {useTypewriter,Cursor} from "react-simple-typewriter"
 
 const Herosection = () => {
   const [text]:any = useTypewriter({
